b44abd683446df8a7cb600d22713f7079457fedf
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-17 20:23:43 +0100

    unittest

--- tests/test_console.py
+++ tests/test_console.py
@@ -45,6 +45,57 @@ def test_class_name(self):
         x = (self.capt_out.getvalue())
         self.assertEqual("** class name missing **\n", x)
 
+@unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "skip db storage")       
+def test_show(self):
+        '''
+            Testing that show exists
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + user_id)
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertTrue(isinstance(x, str))
+
+def test_class_name_doest_exist(self):
+        """ Testing errors if cls doesn't exist"""
+        console = self.create()
+        console.onecmd("create Index")
+        x = (self.capt_out.getvalue())
+        self.assertEqual(" class doesn't exist \n", x)
+        
+def test_show_no_instance_found(self):
+        '''
+            Test show message error for id missing
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + "124356876")
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertEqual("** no instance found **\n", x)
+
+@unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
+def test_create_fileStorage(self):
+    """test filestorage engine"""
+    console = self.create()
+    console.onecmd("create User")
+    self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
 if __name__ == '__main__':
     unittest.main()
 
