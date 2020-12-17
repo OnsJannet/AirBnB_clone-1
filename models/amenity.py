5b2f8a3585db3a0a6372d04a3e4b1f19311b4d25
onsjannet <onsjannet@gmail.com>
2020-12-17 13:57:54 +0100

    added amenity place review state and user

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ The city class, contains state ID and name """    
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -10,3 +12,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
+
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete-orphan")
