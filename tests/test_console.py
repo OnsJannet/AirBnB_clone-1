07c7a854639ccffcf2ebafcc0239d546b871abeb
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-17 20:41:54 +0100

    fix pep8 style for the test

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,48 +7,50 @@ import datetime
 import sys
 from console import HBNBCommand
 
+
 class TestCodeconsole(unittest.TestCase):
+    """class test"""
 
     def test_pep8_conformance(self):
-    """Test that we conform to PEP8."""
-    pep8style = pep8.StyleGuide(quiet=True)
-    result = pep8style.check_files(['models/base_model.py'])
-    self.assertEqual(result.total_errors, 0,
-                     "Found code style errors (and warnings).")
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors .")
 
-def do_quit(self):
-    """ Test Quit """
-    c = self.create()
-    self.assertTrue(c.onecmd("quit"))
+    def setUp(self):
+        '''setup for'''
+        self.backup = sys.stdout
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
 
-def do_EOF(self):
-    """ Test EOF """
-    c = self.create()
-    self.assertTrue(c.onecmd("EOF"))
+    def tearDown(self):
+        ''''''
+        sys.stdout = self.backup
 
-def create(self):
-    """ create an instance of the HBNBCommand class"""
-    return HBNBCommand()
+    def create(self):
+        ''' create an instance of the HBNBCommand class'''
+        return HBNBCommand()
 
-def test_all(self):
-    """ Test all exists """
-    c = self.create()
-    c.onecmd("all")
-    self.assertTrue(isinstance(self.capt_out.getvalue(), str))
-
-def test_class_name(self):
-        """
-            Testing the error msg .
-        """
+    def test_quit(self):
+        ''' Test quit exists'''
         console = self.create()
-        console.onecmd("create")
-        x = (self.capt_out.getvalue())
-        self.assertEqual("** class name missing **\n", x)
+        self.assertTrue(console.onecmd("quit"))
 
-@unittest.skipIf(
+    def test_EOF(self):
+        ''' Test EOF exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
+
+    def test_all(self):
+        ''' Test all exists'''
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
+    @unittest.skipIf(
         os.getenv('HBNB_TYPE_STORAGE') == 'db',
-        "skip db storage")       
-def test_show(self):
+        "won't work in db")
+    def test_show(self):
         '''
             Testing that show exists
         '''
@@ -64,14 +66,48 @@ def test_show(self):
         sys.stdout = self.backup
         self.assertTrue(isinstance(x, str))
 
-def test_class_name_doest_exist(self):
-        """ Testing errors if cls doesn't exist"""
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
+    def test_show_class_name(self):
+        '''
+            Testing the error messages for class name missing.
+        '''
         console = self.create()
-        console.onecmd("create Index")
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show")
         x = (self.capt_out.getvalue())
-        self.assertEqual(" class doesn't exist \n", x)
-        
-def test_show_no_instance_found(self):
+        sys.stdout = self.backup
+        self.assertEqual("** class name missing **\n", x)
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
+    def test_show_class_name(self):
+        '''
+            Test show message error for id missing
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User")
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertEqual("** instance id missing **\n", x)
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
+    def test_show_no_instance_found(self):
         '''
             Test show message error for id missing
         '''
@@ -87,15 +123,31 @@ def test_show_no_instance_found(self):
         sys.stdout = self.backup
         self.assertEqual("** no instance found **\n", x)
 
-@unittest.skipIf(
+    @unittest.skipIf(
         os.getenv('HBNB_TYPE_STORAGE') == 'db',
         "won't work in db")
-def test_create_fileStorage(self):
-    """test filestorage engine"""
-    console = self.create()
-    console.onecmd("create User")
-    self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+    def test_create_fileStorage(self):
+        '''
+            Test that create works
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_class_name(self):
+        '''
+            Testing the error messages for class name missing.
+        '''
+        console = self.create()
+        console.onecmd("create")
+        x = (self.capt_out.getvalue())
+        self.assertEqual("** class name missing **\n", x)
 
+    def test_class_name_doest_exist(self):
+        '''
+            Testing the error messages for class name missing.
+        '''
+        console = self.create()
+        console.onecmd("create Ons")
+        x = (self.capt_out.getvalue())
+        self.assertEqual("** class doesn't exist **\n", x)
