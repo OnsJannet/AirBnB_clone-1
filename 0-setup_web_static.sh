87a678d7505a9c8871178e865370c090761cbc1d
onsjannet <onsjannet@gmail.com>
2020-12-22 19:02:51 +0100

    correcting output

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,8 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 sudo echo "<html>
   <head>
   </head>
@@ -12,7 +13,7 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html 
 sudo rm -f -R /data/web_static/current
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 MSG="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "53i $MSG" /etc/nginx/sites-available/default
