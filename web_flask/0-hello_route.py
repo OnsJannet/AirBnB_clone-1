6aee9c55aa0f031e56b39a49d954892c50c7aa89
onsjannet <onsjannet@gmail.com>
2021-01-18 19:39:13 +0100

    added doc + strict_slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+''' Flask web application '''
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
