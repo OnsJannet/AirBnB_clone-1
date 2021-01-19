8ab2b9a6ff7370e4f2ae84af634f1dab6a193312
onsjannet <onsjannet@gmail.com>
2021-01-19 21:42:16 +0100

    correcting output using relationship

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,9 +9,9 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State)
+@app.route('/cities_by_state', strict_slashes=False)
+def cities_by_state():
+    all_states = storage.all(State)
     return render_template('8-cities_by_states.html', states=all_states)
 
 
@@ -20,5 +20,5 @@ def teardown_appcontext(self):
     return storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
