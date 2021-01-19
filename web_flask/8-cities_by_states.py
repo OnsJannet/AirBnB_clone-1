9486f930291bec8de6ead15e4910a30328776bcd
onsjannet <onsjannet@gmail.com>
2021-01-19 16:47:37 +0100

    forgot to import city so i imported it

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask import render_template
 from models.state import State
+from models.city import City
 from models import storage
 
 app = Flask(__name__)
@@ -13,7 +14,8 @@ app = Flask(__name__)
 def cities_list():
     states = storage.all(State)
     cities = storage.all(City)
-    return render_template('8-cities_by_states.html', states=all_states, cities=all_cities)
+    return render_template('8-cities_by_states.html', states=all_states, 
+                           cities=all_cities)
 
 
 @app.teardown_appcontext
