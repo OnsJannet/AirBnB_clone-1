c5b1e375b04f6541906dc122600a5ed69c76140e
onsjannet <onsjannet@gmail.com>
2021-01-19 20:40:53 +0100

    correcting output

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,7 +4,6 @@
 from flask import Flask
 from flask import render_template
 from models.state import State
-from models.city import City
 from models import storage
 
 app = Flask(__name__)
@@ -13,9 +12,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State)
-    cities = storage.all(City)
-    return render_template('8-cities_by_states.html', states=all_states,
-                           cities=all_cities)
+    return render_template('8-cities_by_states.html', states=all_states)
 
 
 @app.teardown_appcontext
