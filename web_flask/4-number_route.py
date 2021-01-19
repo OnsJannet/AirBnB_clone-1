95f7773acc1b32ac138db820c9c77dcba2fae225
onsjannet <onsjannet@gmail.com>
2021-01-19 12:40:57 +0100

    added @app.route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,26 +4,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return("Hello HBNB!") 
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return("HBNB")
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    return "C {}".format(text.replace('_', ' '))
+    return "C %s" % text.replace('_', ' ')
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
-    return "Python {}".format(text.replace('_', ' '))
+    return "Python %s" % text.replace('_', ' ')
 
+
+@app.route('/number/', strict_slashes=False)
 @app.route('/number/<n>', strict_slashes=False)
 def number(n=int):
-    return "{} is a number".format(n)
-
+    return "%d is a number" % n
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
