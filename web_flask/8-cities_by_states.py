cc7460364347d8553d6766a5576132d4c088af5b
onsjannet <onsjannet@gmail.com>
2021-01-19 15:57:59 +0100

    added 8-cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Flask web application '''
+
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_list():
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=all_states,
+                           cities=all_cities)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    return storage.close()
